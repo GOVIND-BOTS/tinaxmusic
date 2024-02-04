9432747404c61408e7eb8153f3eda98eafd684f0
synaxxlove <158541689+synaxxlove@users.noreply.github.com>
2024-02-04 08:50:14 +0530

    Update couples.py

--- AnonXMusic/plugins/management/couples.py
+++ AnonXMusic/plugins/management/couples.py
@@ -8,8 +8,8 @@ from pyrogram.types import *
 from pyrogram.enums import *
 
 #BOT FILE NAME
-from AloneXMusic import app as app
-from AloneXMusic.mongo.couples_db import _get_image, get_couple
+from AnonXMusic import app as app
+from AnonXMusic.mongo.couples_db import _get_image, get_couple
 
 def dt():
     now = datetime.now()
